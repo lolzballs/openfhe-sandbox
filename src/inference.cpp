2c1f8629f383a1394fd9434f4d3e22d783f29308
Benjamin Cheng <ben@bcheng.me>
2022-12-10 16:31:10 -0500

    inference: test reading idx

--- src/inference.cpp
+++ src/inference.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char **argv) {
 	lbcrypto::CCParams<lbcrypto::CryptoContextCKKSRNS> parameters;
 	auto weights = ndarray<double>::load_from_file("lr_weights.bin", { 28 * 28 });
+	auto images = ndarray<uint8_t>::load_from_idx_file(".data/mnist_trimmed/"
+			"t10k-images-idx3-ubyte");
 
+	std::cout << images.shape.size() << std::endl;
 	std::cout << weights[{ 600 }] << std::endl;
 }
