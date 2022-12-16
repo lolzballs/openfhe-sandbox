1eab7f7d6fb3057243fc0884b08dcd87259a432c
Benjamin Cheng <ben@bcheng.me>
2022-12-16 13:49:54 -0500

    inference: new mode for evaluating whole dataset

--- src/inference.cpp
+++ src/inference.cpp
@@ -19,7 +19,7 @@ static Ciphertext inner_product(const CryptoContext &cc,
 }
 
 static Ciphertext predict(const CryptoContext &cc,
-		const Ciphertext &&features, const Ciphertext &&weights,
+		const Ciphertext &features, const Ciphertext &weights,
 		std::optional<Ciphertext*> intermediate = std::nullopt) {
 	auto dot = inner_product(cc, features, weights);
 	if (intermediate.has_value())
@@ -39,21 +39,24 @@ static Ciphertext predict(const CryptoContext &cc,
 	return cc->EvalPoly(dot, coeffs);
 }
 
+static void print_usage(char *program) {
+	fprintf(stderr, "usage: %s <image_idx>\n", program);
+}
+
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s <image_idx>\n", argv[0]);
+	if (argc != 1 && argc != 2) {
+		print_usage(argv[0]);
 		return -1;
 	}
-	std::size_t image_idx = std::strtoull(argv[1], NULL, 10);
 
-	auto weights = ndarray<double>::load_from_file("lr_weights.bin", { 28 * 28 });
-	auto images = ndarray<uint8_t>::load_from_idx_file(".data/mnist_trimmed/"
+	const auto weights = ndarray<double>::load_from_file("lr_weights.bin", { 28 * 28 });
+	const auto images = ndarray<uint8_t>::load_from_idx_file(".data/mnist_trimmed/"
 			"t10k-images-idx3-ubyte").to<double>();
 
 	/* setup crypto context */
-	uint32_t mult_depth = 4;
-	uint32_t scale_mod_size = 50;
-	uint32_t batch_size = 1 << ceil_log2(28 * 28);
+	const uint32_t mult_depth = 4;
+	const uint32_t scale_mod_size = 50;
+	const uint32_t batch_size = 1 << ceil_log2(28 * 28);
 	lbcrypto::CCParams<lbcrypto::CryptoContextCKKSRNS> parameters;
 	parameters.SetMultiplicativeDepth(mult_depth);
 	parameters.SetScalingModSize(scale_mod_size);
@@ -69,39 +72,69 @@ int main(int argc, char **argv) {
 		<< cc->GetRingDimension() << std::endl;
 
 	/* key generation */
-	auto keys = cc->KeyGen();
+	const auto keys = cc->KeyGen();
 	std::cout << "generating multiplication keys" << std::endl;
 	cc->EvalMultKeysGen(keys.secretKey);
-
 	cc->EvalSumKeyGen(keys.secretKey);
 
 	std::cout << "encoding weights" << std::endl;
-	auto weight_span = weights[std::nullopt];
-	lbcrypto::Plaintext ptw = cc->MakeCKKSPackedPlaintext(
+	const auto weight_span = weights[std::nullopt];
+	const lbcrypto::Plaintext ptw = cc->MakeCKKSPackedPlaintext(
 			weight_span.begin(), weight_span.end());
 
-	auto image = images[image_idx];
-	std::cout << "encoding image" << std::endl;
-	lbcrypto::Plaintext pti = cc->MakeCKKSPackedPlaintext(
-			image.begin(), image.end());
-
 	std::cout << "encrypting weights" << std::endl;
-	Ciphertext ctw = cc->Encrypt(keys.publicKey, ptw);
-	std::cout << "encrypting image" << std::endl;
-	Ciphertext cti = cc->Encrypt(keys.publicKey, pti);
+	const Ciphertext ctw = cc->Encrypt(keys.publicKey, ptw);
 
-	std::cout << "predicting..." << std::endl;
-	Ciphertext before_sigmoid;
-	Ciphertext prediction = predict(cc, std::move(cti), std::move(ctw),
-			&before_sigmoid);
+	if (argc == 2) {
+		std::size_t image_idx = std::strtoull(argv[1], NULL, 10);
+		const auto image = images[image_idx];
+		std::cout << "encoding image" << std::endl;
+		const lbcrypto::Plaintext pti = cc->MakeCKKSPackedPlaintext(
+				image.begin(), image.end());
+		std::cout << "encrypting image" << std::endl;
+		const Ciphertext cti = cc->Encrypt(keys.publicKey, pti);
 
-	lbcrypto::Plaintext pt_before_sigmoid;
-	cc->Decrypt(before_sigmoid, keys.secretKey, &pt_before_sigmoid);
-	std::cout << "intermediate " << " "
-		<< pt_before_sigmoid->GetRealPackedValue()[0] << std::endl;
+		std::cout << "predicting..." << std::endl;
+		Ciphertext before_sigmoid;
+		const Ciphertext prediction = predict(cc, cti, ctw,
+				&before_sigmoid);
 
-	lbcrypto::Plaintext pt_prediction;
-	auto res = cc->Decrypt(prediction, keys.secretKey, &pt_prediction);
-	std::cout << "prediction " << res.isValid
-		<< " " << pt_prediction->GetRealPackedValue()[0] << std::endl;
+		lbcrypto::Plaintext pt_before_sigmoid;
+		cc->Decrypt(prediction, keys.secretKey, &pt_before_sigmoid);
+		std::cout << "before sigmoid "
+			<< pt_before_sigmoid->GetRealPackedValue()[0] << std::endl;
+
+		lbcrypto::Plaintext pt_prediction;
+		cc->Decrypt(prediction, keys.secretKey, &pt_prediction);
+		std::cout << "prediction "
+			<< pt_prediction->GetRealPackedValue()[0] << std::endl;
+
+	} else {
+		const auto labels = ndarray<uint8_t>::load_from_idx_file(
+				".data/mnist_trimmed/t10k-labels-idx1-ubyte");
+		std::size_t correct = 0;
+
+		/* TODO: implement batching */
+		for (std::size_t i = 0; i < images.shape[0]; i++) {
+			const auto image = images[i];
+			const uint8_t ground_truth = *labels[i].begin();
+
+			std::cout << "predicting image " << i << std::endl;
+			const lbcrypto::Plaintext pti = cc->MakeCKKSPackedPlaintext(
+					image.begin(), image.end());
+			const Ciphertext cti = cc->Encrypt(keys.publicKey, pti);
+
+			const Ciphertext prediction = predict(cc, cti, ctw);
+
+			lbcrypto::Plaintext pt_prediction;
+			cc->Decrypt(prediction, keys.secretKey, &pt_prediction);
+			if (((pt_prediction->GetRealPackedValue()[0] >= 0.5) ? 3 : 8) == ground_truth)
+				correct++;
+		}
+
+		std::cout << "accuracy: "
+			<< static_cast<double>(correct) / images.shape[0] << std::endl;
+	}
+
+	return 0;
 }
